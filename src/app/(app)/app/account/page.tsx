44b2b3985d1d3e479a4524e22d7eb055cfbe14c1
TusharSSurve <ttusharsurve567@gmail.com>
2025-03-26 21:59:21 +0530

    [feat] : user email

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,12 +1,15 @@
 import ContentBlock from "@/components/ContentBlock";
 import H1 from "@/components/H1";
+import { auth } from "@/lib/auth";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
       <ContentBlock className="h-[500px] flex justify-center items-center">
-        <p>Logged in as...</p>
+        <p>Logged in as {session?.user?.email}</p>
       </ContentBlock>
     </main>
   )
